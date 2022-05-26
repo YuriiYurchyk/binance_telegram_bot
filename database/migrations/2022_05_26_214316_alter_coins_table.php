6b744b1304570d80f2b6496a10358e9df3e377d5
Yurii Yurchyk <masterjurij@gmail.com>
2022-05-26 21:46:20 +0300

    fix

--- database/migrations/2022_05_26_214316_alter_coins_table.php
+++ database/migrations/2022_05_26_214316_alter_coins_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('coins', function (Blueprint $table) {
             $table->string('google_alerts_url')->nullable()->change();
-            $table->boolean('google_alerts')->default(false)->change();
+            $table->boolean('google_alerts')->default(0)->change();
         });
     }
 
