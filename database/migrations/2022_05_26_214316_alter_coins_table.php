005aad7dccc0837034015965e04312c641a3f51e
Yurii Yurchyk <masterjurij@gmail.com>
2022-05-26 21:44:25 +0300

    fix

---
+++ database/migrations/2022_05_26_214316_alter_coins_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('coins', function (Blueprint $table) {
+            $table->string('google_alerts_url')->nullable()->change();
+            $table->boolean('google_alerts')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('coins', function (Blueprint $table) {
+            $table->string('google_alerts_url')->change();
+            $table->boolean('google_alerts')->change();
+        });
+    }
+};
